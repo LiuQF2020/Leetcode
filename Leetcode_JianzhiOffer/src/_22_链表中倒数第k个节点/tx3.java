6a81a67d71b52461b46c0799f421e75a4501f3b1
LQF <41264429+LiuQF2020@users.noreply.github.com>
2020-08-23 21:07:28 +0800

    Create tx3.java

---
+++ Leetcode_JianzhiOffer/src/_22_链表中倒数第k个节点/tx3.java
@@ -0,0 +1,39 @@
+package written_examination.Tencent;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-- > 0){
+            int n = sc.nextInt();
+            System.out.println(unknownMethod(n));
+//            System.out.println(rightMethod(n));
+        }
+    }
+    public static int unknownMethod(int n){
+        int mod = n % 10;
+
+        return getDigitSum(n -  mod - 1) + 1 + mod;
+    }
+
+//    public static int rightMethod(int n){
+//        int maxSum = Integer.MIN_VALUE;
+//        for(int i = 0; i <= n; i++){
+//            int temp = getDigitSum(i) + getDigitSum(n - i);
+//            maxSum = Math.max(temp,maxSum);
+//        }
+//        return maxSum;
+//    }
+
+    public static int getDigitSum(int num){
+        String str = String.valueOf(num);
+        int result = 0;
+        for(int i = 0; i < str.length(); i++){
+            result += (str.charAt(i) - '0');
+        }
+        return result;
+    }
+}

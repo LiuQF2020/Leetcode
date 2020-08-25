f9182d7074de526568061bc2d1f1b0c133d8b730
LQF2020 <1759768033@qq.com>
2020-08-25 16:43:10 +0800

    n

---
+++ Leetcode_JianzhiOffer/src/_35_复杂链表的复制/Solution.java
@@ -0,0 +1,19 @@
+package _35_复杂链表的复制;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class Solution {
+    public Node copyRandomList(Node head) {
+        return head;
+    }
+}

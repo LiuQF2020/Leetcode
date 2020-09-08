ba5587aa387dd86aa2afbc698e675e094d538a3e
LQF <liuqifeng19960918@outlook.com>
2020-09-08 16:52:17 +0800

    手撕代码

--- Leetcode_JianzhiOffer/src/_45_把数组排成最小的数/Solution.java
+++ Leetcode_JianzhiOffer/src/_45_把数组排成最小的数/Solution.java
@@ -4,24 +4,17 @@ import java.util.Arrays;
 
 public class Solution {
     public String minNumber(int[] nums) {
-        String[] strs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            strs[i] = String.valueOf(nums[i]);
-        }
-
-        Arrays.sort(strs,(o1,o2) -> (o1+o2).compareTo(o2+o1));
-        StringBuilder sb = new StringBuilder();
-        for (String s : strs) {
-            sb.append(s);
-        }
-        return sb.toString();
+        return " ";
     }
 
-    public static void main(String[] args) {
-        int[] arr = {10, 9, 13, 7, 8, 6, 21};
-        Solution sl = new Solution();
-        String strs = sl.minNumber(arr);
 
-        System.out.println(strs);
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        int[] arr = new int[] {3, 8, 4, 1, 9};
+        String str = "";
+        str = sl.minNumber(arr);
+
+        System.out.println(str);
     }
 }
---
+++ Scratch_Code/src/_1_二分查找/BinarySearch.java
@@ -0,0 +1,5 @@
+package _1_二分查找;
+
+public class BinarySearch {
+
+}

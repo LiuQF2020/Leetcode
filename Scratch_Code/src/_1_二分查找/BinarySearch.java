251f5995b95bf7475c1b24f1015536c0788bd3db
LQF <liuqifeng19960918@outlook.com>
2020-09-08 18:33:41 +0800

    二分查找

--- Scratch_Code/src/_1_二分查找/BinarySearch.java
+++ Scratch_Code/src/_1_二分查找/BinarySearch.java
@@ -2,4 +2,34 @@ package _1_二分查找;
 
 public class BinarySearch {
 
+    /**
+     * 非递归实现
+     * @param arr ：有序数组
+     * @param key ：需要查找的数
+     * @return ：返回key在数组arr中的下标
+     */
+    public static int binarySearch(int[] arr, int key) {
+        if (arr.length < 1)     return -1;
+        int start = 0, mid, end = arr.length - 1;
+
+        while (start <= end) {
+            // 防止int溢出
+            mid = start + (end - start) / 2;
+
+            if (arr[mid] > key) {
+                end = mid + 1;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 9};
+        System.out.println(binarySearch(arr, 8));
+    }
 }
---
+++ Scratch_Code/src/_1_二分查找/BinarySearchWithRecursion.java
@@ -0,0 +1,40 @@
+package _1_二分查找;
+
+public class BinarySearchWithRecursion {
+
+    /**
+     * 递归实现
+     * @param arr ： 有序数组
+     * @param key ：需要查找的数
+     * @return ：返回key在数组array中的下标
+     */
+    public static int binarySearch(int[] arr, int key) {
+        if (arr.length < 1) {
+            return -1;
+        }
+
+        int index = binarySearch(arr, key, 0, arr.length - 1);
+        return index;
+    }
+
+    public static int binarySearch(int[] arr, int key, int start, int end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+
+        if (start >= end) {
+            return -1;
+        } else if (arr[mid] > key) {
+            return binarySearch(arr, key, start, mid - 1);
+        } else if (arr[mid] < key) {
+            return binarySearch(arr, key, mid + 1, end);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 9};
+        System.out.println(binarySearch(arr, 8));
+    }
+}

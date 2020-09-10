98dde7989f6405e901a6a4292d7d7e900532a6d3
LQF <liuqifeng19960918@outlook.com>
2020-09-10 20:04:25 +0800

    前序遍历、中序遍历、后序遍历这个方法太牛逼了。。。。力扣颜色标记法题解下的某个Java实现的评论。

--- Scratch_Code/src/_07_树/_3_层序遍历实现/LevelTraverse.java
+++ Scratch_Code/src/_07_树/_3_层序遍历实现/LevelTraverse.java
@@ -1,4 +1,41 @@
 package _07_树._3_层序遍历实现;
 
+import java.util.LinkedList;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+
+
+/*
+只需要增加一个队列，将遍历过程中的节点依次放进去;
+再去按照队列中的节点去遍历它们的子节点。
+ */
 public class LevelTraverse {
+    public void levelTraverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.val + " ");
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+
+        }
+    }
 }

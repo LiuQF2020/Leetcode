cd33f498fba1c6def5a1c657a05e76c2f34f4ca4
LQF2020 <1759768033@qq.com>
2020-09-20 21:17:35 +0800

    du

--- Leetcode_JianzhiOffer/src/Test/Main.java
+++ Leetcode_JianzhiOffer/src/Test/Main.java
@@ -1,7 +1,27 @@
 package Test;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Set<Character> set = new HashSet<>();
 
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+//        sc.nextLine();
+        String str2 = sc.nextLine();
+
+        for (int i = 0; i < str1.length(); i++) {
+            set.add(str1.charAt(i));
+        }
+        int j = 0;
+        for (int i = 0; i < str2.length(); i++) {
+            if (set.contains(str2.charAt(i))) {
+                j++;
+            }
+        }
+        System.out.println(j);
     }
 }

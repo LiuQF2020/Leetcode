e9971cde68b02797cfd8c034e09263dc5b83137e
LQF <liuqifeng19960918@outlook.com>
2020-09-22 07:58:56 +0800

    回文Test

--- Leetcode_Hot_100/src/_125_验证回文串/Solution.java
+++ Leetcode_Hot_100/src/_125_验证回文串/Solution.java
@@ -10,7 +10,6 @@ public class Solution {
 
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
             i++;
-
             j--;
         }
         return true;
@@ -20,5 +19,21 @@ public class Solution {
         Solution sl = new Solution();
         String str = "w .....)))ew`";
         System.out.println(sl.isPalindrome(str));
+    }
+
+    public boolean isPa(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(str.charAt(i)))  i++;
+            while (i < j && !Character.isLetterOrDigit(str.charAt(j)))  j--;
+
+            if (Character.toLowerCase(str.charAt(i)) != Character.toLowerCase(str.charAt(j)))  return false;
+
+            i++;
+            j--;
+        }
+        return true;
     }
 }

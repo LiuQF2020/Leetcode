1f92495871116d9fd0f17927a54ed7eaa5cba31b
LQF <liuqifeng19960918@outlook.com>
2020-09-22 22:55:52 +0800

    1

--- Scratch_Code/src/_00_排序算法/_1_冒泡排序/BubbleSort.java
+++ Scratch_Code/src/_00_排序算法/_1_冒泡排序/BubbleSort.java
@@ -1,4 +1,9 @@
 package _00_排序算法._1_冒泡排序;
 
 public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            
+        }
+    }
 }

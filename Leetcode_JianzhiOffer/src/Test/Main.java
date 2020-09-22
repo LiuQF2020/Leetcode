61012487668ef2b4590e5845f0fa2d9a753318a4
LQF2020 <1759768033@qq.com>
2020-09-22 22:46:15 +0800

    丑数

--- Leetcode_JianzhiOffer/src/Test/Main.java
+++ Leetcode_JianzhiOffer/src/Test/Main.java
@@ -1,24 +1,31 @@
 package Test;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Character> set = new HashSet<>();
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
 
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
-//        sc.nextLine();
         String str2 = sc.nextLine();
 
         for (int i = 0; i < str1.length(); i++) {
-            set.add(str1.charAt(i));
+            set1.add(str1.charAt(i));
         }
         int j = 0;
         for (int i = 0; i < str2.length(); i++) {
-            if (set.contains(str2.charAt(i))) {
+            set2.add(str1.charAt(i));
+        }
+
+        Iterator iter2 = set2.iterator();
+        while (iter2.hasNext()) {
+            Character i = (Character) iter2.next();
+            if (set1.contains(i)) {
                 j++;
             }
         }
--- Leetcode_JianzhiOffer/src/_45_把数组排成最小的数/Solution.java
+++ Leetcode_JianzhiOffer/src/_45_把数组排成最小的数/Solution.java
@@ -1,10 +1,22 @@
 package _45_把数组排成最小的数;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Solution {
     public String minNumber(int[] nums) {
-        return " ";
+        String[] str = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            str[i] = String.valueOf(nums[i]);
+        }
+
+        // 这个结构在哪里还能用的上？
+        Arrays.sort(str, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder sb = new StringBuilder();
+        for (String s : str) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 
 
@@ -12,9 +24,9 @@ public class Solution {
         Solution sl = new Solution();
 
         int[] arr = new int[] {3, 8, 4, 1, 9};
-        String str = "";
-        str = sl.minNumber(arr);
+        String[] strs = new String[] {"30","10", "1", "3"};
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
 
-        System.out.println(str);
+        System.out.println(Arrays.toString(strs));
     }
 }

a97545ff24d4912145f58e3319016123d7352861
LQF <liuqifeng19960918@outlook.com>
2020-10-04 16:21:19 +0800

    找到出现1次的数字1

--- Leetcode_JianzhiOffer/src/_56_1_数组中数字出现的次数/Solution.java
+++ Leetcode_JianzhiOffer/src/_56_1_数组中数字出现的次数/Solution.java
@@ -1,4 +1,4 @@
-package _56_2_数组中数字出现的次数;
+package _56_1_数组中数字出现的次数;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,17 +6,19 @@ import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    public int singleNumber(int[] nums) {
+    public int[] singleNumbers(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        int res = 0;
+        int[] res = new int[2];
         for (int num : nums) {
             if (map.containsKey(num))  map.put(num, 0);
             else map.put(num, 1);
         }
 
+        int i = 0;
         for(Object key: map.keySet()){
             if(map.get(key).equals(1)){
-                res = (int) key;
+                res[i] = (int) key;
+                i++;
             }
         }
         return res;

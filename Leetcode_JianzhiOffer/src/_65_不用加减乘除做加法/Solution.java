517c3cd9f922ec31d90a188c99baec45635f8647
LQF <liuqifeng19960918@outlook.com>
2020-10-04 19:41:48 +0800

    异或、进位实现加法。

--- Leetcode_JianzhiOffer/src/_65_不用加减乘除做加法/Solution.java
+++ Leetcode_JianzhiOffer/src/_65_不用加减乘除做加法/Solution.java
@@ -1,5 +1,10 @@
 package _65_不用加减乘除做加法;
 
 public class Solution {
-
+    public int add(int a, int b) {
+        if(b == 0)  return a;
+        int c = a ^ b;
+        int d = (a & b) << 1;
+        return add(c, d);
+    }
 }

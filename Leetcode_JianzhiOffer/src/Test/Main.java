ed3f9f5b2a10fcbb594698874fccd308acde69d5
LQF <liuqifeng19960918@outlook.com>
2020-12-11 14:53:01 +0800

    1

--- Leetcode_JianzhiOffer/src/Test/Main.java
+++ Leetcode_JianzhiOffer/src/Test/Main.java
@@ -1,14 +1,9 @@
 package Test;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] {0, 2};
-        System.out.println(Arrays.toString(arr));
+
     }
 }
+
+
---
+++ Leetcode_Problems/src/_0105_从前序与中序遍历序列构造二叉树/Solution.java
@@ -0,0 +1,14 @@
+package _0105_从前序与中序遍历序列构造二叉树;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+    }
+}

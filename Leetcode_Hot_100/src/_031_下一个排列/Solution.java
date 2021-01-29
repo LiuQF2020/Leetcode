8aa7da54ebe84f3afcc8db8ba0b9360c066cb3f4
LQF <liuqifeng19960918@outlook.com>
2021-01-29 19:50:41 +0800

    1

--- Leetcode_Hot_100/src/_031_下一个排列/Solution.java
+++ Leetcode_Hot_100/src/_031_下一个排列/Solution.java
@@ -4,46 +4,17 @@ import java.util.Arrays;
 
 public class Solution {
     public static void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int temp;
-        int[] nums_1 = Arrays.copyOf(nums, n);
-
+        int[] ans;
+        int[] pre_nums = Arrays.copyOf(nums, nums.length);
+        int[] pre_nums_revers = new int[nums.length];
         Arrays.sort(nums);
-
-        int flag = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == nums_1[n - 1 - i]) {
-                flag++;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            pre_nums_revers[i] = pre_nums[nums.length - i];
         }
 
-        if (flag == n) {
-            for (int i = 0; i < n; i++) {
-                nums[i] = nums_1[n - 1 - i];
-            }
-
-        }
-
-        else {
-            for (int i = n - 1; i > 0; i--) {
-                if (nums_1[i] <= nums_1[i - 1]) {
-                    continue;
-                }
-                else {
-                    temp = nums_1[i];
-                    nums_1[i] = nums_1[i - 1];
-                    nums_1[i - 1] = temp;
-                    break;
-                }
-            }
-        }
-
-
+        if (Arrays.equals(nums, pre_nums_revers))  nums = pre_nums_revers;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 5, 9};
-        nextPermutation(nums);
-        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ Leetcode_Hot_100/src/_034_在排序数组中查找元素的第一个和最后一个位置/Solution.java
@@ -0,0 +1,7 @@
+package _034_在排序数组中查找元素的第一个和最后一个位置;
+
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[] {0};
+    }
+}
